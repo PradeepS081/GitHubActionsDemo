3bdcb6e326a1e0a539f43a3e571136ee959a9142
PradeepS081 <pradeeps@nousinfo.com>
2024-09-10 23:47:35 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,18 +1,2 @@
-// Import the Express module
-const express = require('express');
 
-// Create an instance of an Express application
-const app = express();
-
-// Define a port number
-const port = 3000;
-
-// Define a route for the root URL ('/')
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
-// Start the server and listen on the defined port
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+console.log("Hello, GitHub Actions!");
